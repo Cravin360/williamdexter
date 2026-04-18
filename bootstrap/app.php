7c12bf2ea4cad905556df4e4a8550a333e5cfc79
treyfatech-star <treyfatech@gmail.com>
2026-04-18 22:51:08 +0100

    Content Error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -40,10 +40,6 @@ $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
     App\Exceptions\Handler::class
 );
-
-->withMiddleware(function (Middleware $middleware) {
-    $middleware->trustProxies(at: '*');
-});
 /*
 |--------------------------------------------------------------------------
 | Return The Application
